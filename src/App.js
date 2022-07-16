94b04f68be83773619ba3b761a39cb84f0df471b
fabio-sv <fabio_dev@outlook.com>
2022-07-15 23:06:05 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movieList, setMovieList] = useState;
+
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+        };
+        loadAll();
+    }, []);
+
+    return (
+        <div className="page">
+            <p>Hello World</p>
+        </div>
+    );
 }
 
 export default App;
+
+//44:17
